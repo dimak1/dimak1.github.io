420bac11702ad8fc4496ec268e3b71436fd427b5
Dima K <dima1086@hotmail.com>
2020-03-10 23:38:06 -0700

    Add config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dima K`,
+    description: `Personal professional website`,
+    author: `Dima Kostenyuk`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-antd`,
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `data`,
+        path: `${__dirname}/src/data/`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,7 +27,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
